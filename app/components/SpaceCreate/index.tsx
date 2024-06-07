dd718f6d58700ab645881fc13f632930c99aacf2
mgkim <mgkim@hu1.com>
2024-06-07 15:09:59 +0900

    refactor: Update SpaceCreate component styles and functionality

--- app/components/SpaceCreate/index.tsx
+++ app/components/SpaceCreate/index.tsx
@@ -16,6 +16,10 @@ import LabelBox from "../Box/LabelBox";
 import TextInput from "../Box/TextInput";
 import { spaceCreateStyle } from "./styles.css";
 
+interface ClickedItem {
+  email: string;
+}
+
 export default function SpaceCreate() {
   const navigate = useNavigate();
   const fetcher = useFetcher<typeof loader>();
@@ -23,30 +27,47 @@ export default function SpaceCreate() {
   const [memberData, setMemberData] = useState<
     MemberSearchType[] | undefined | null
   >();
+  const [isFetchMembersDisabled, setIsFetchMembersDisabled] = useState(false);
 
   useEffect(() => {
-    if (fetcher.state === "idle") {
+    if (fetcher.state === "idle" && !isFetchMembersDisabled) {
       setMemberData(fetcher.data?.response);
       console.log(memberData);
     }
-  }, [fetcher, memberData]);
+  }, [fetcher, memberData, isFetchMembersDisabled]);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedFetchMembers = useCallback(
-    debounce((search: string) => {
+    debounce((keyword: string) => {
       const formData = new FormData();
-      formData.append("search", search);
+      formData.append("keyword", keyword);
+      console.log(keyword);
       fetcher.submit(formData, {
-        action: `/api/get-member-live?search=${search}`,
+        action: `/api/get-member-live?keyword=${keyword}`,
       });
-    }, 300), // 300ms 지연
+    }, 300),
     []
   );
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsFetchMembersDisabled(false);
     const value = e.target.value;
     setState("ownerId", value);
     debouncedFetchMembers(value);
+  };
+
+  const handleDropDownKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (state.ownerId && memberData?.length) {
+      if (e.key === "ArrowDown") {
+        e.preventDefault();
+      }
+    }
+  };
+
+  const clickDropDownItem = (clickedItem: ClickedItem) => {
+    setState("ownerId", clickedItem.email);
+    setMemberData(null);
+    setIsFetchMembersDisabled(true);
   };
 
   return (
@@ -78,8 +99,25 @@ export default function SpaceCreate() {
           <TextInput
             placeholder="계정 검색"
             wrapSprinkles={{ width: "100%" }}
+            value={state.ownerId}
             onChange={handleSearchChange}
+            onKeyUp={handleDropDownKey}
           ></TextInput>
+          <Box>
+            {memberData && (
+              <ul className={spaceCreateStyle.ul}>
+                {memberData?.map((member, index) => (
+                  <li
+                    className={spaceCreateStyle.li}
+                    onClick={() => clickDropDownItem(member)}
+                    key={index}
+                  >
+                    {member.email}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </Box>
         </Box>
         <Box>
           <LabelBox label={"만료일 지정"} />
@@ -87,7 +125,6 @@ export default function SpaceCreate() {
             value={state.expiredAt}
             onChange={(date) => {
               setState("expiredAt", date.isoDate);
-              console.log(date);
             }}
           ></CalendarInput>
         </Box>
--- app/components/SpaceCreate/styles.css.ts
+++ app/components/SpaceCreate/styles.css.ts
@@ -1,9 +1,21 @@
 import { getTextStyles } from "~/styles/helpers";
 import { appStyles } from "~/styles/layer.css";
+import { vars } from "~/styles/vars.css";
 
 export const spaceCreateStyle = appStyles({
   title: {
     ...getTextStyles("Grayscale/Black", "Title/32px/32px.700"),
     marginBottom: "24px",
+  },
+  ul: {
+    border: `1px solid ${vars.colors["Grayscale/Gray 100"]}`,
+    borderRadius: "8px",
+    padding: "8px 16px",
+  },
+  li: {
+    ...getTextStyles("Grayscale/Black", "Body/16px/16px.400"),
+    ":hover": {
+      backgroundColor: vars.colors["Grayscale/Gray 100"],
+    },
   },
 });
